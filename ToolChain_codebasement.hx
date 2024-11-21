7c02fe2adf78c720b66853422d8cf3fb640a0aed
David Walley <1883160+DavidWalley@users.noreply.github.com>
2024-11-21 14:06:15 -0600

    cleanup

---
+++ ToolChain_codebasement.hx
@@ -0,0 +1,6 @@
+// codebasement/ToolChain_codebasement.hx - Haxe language BODGE code for codebasement project.
+// (c)2024 David C. Walley
+
+// THE FOLLOWING IS NOT SOURCE CODE. THESE ARE MY NOTES!
+
+
